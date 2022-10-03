18302ef2c7aad5192b2490ffa15429fdcf3613f4
svetlana <nooch@yandex.ru>
2022-10-04 01:55:27 +0300

    добавлен алгоритм нахождения максимального числа из 3 чисел

--- Program.cs
+++ Program.cs
@@ -9,3 +9,14 @@ Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine($"максимальное число - {max}");
